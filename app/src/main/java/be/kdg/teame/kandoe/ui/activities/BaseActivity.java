9f77eef01bb7b95614b1f71d3f58b60a2e7d1484
Arne De Cock <arne.decock.1@student.kdg.be>
2016-02-07 22:19:31 +0100

    Integration of the toolbar (remarks - look at StatusBar transparency)

--- app/src/main/java/be/kdg/teame/kandoe/ui/activities/BaseActivity.java
+++ app/src/main/java/be/kdg/teame/kandoe/ui/activities/BaseActivity.java
@@ -2,6 +2,9 @@ package be.kdg.teame.kandoe.ui.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import be.kdg.teame.kandoe.R;
 
 /**
  * Created by Arne on 04/02/2016.
@@ -11,6 +14,8 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutResource());
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     protected abstract int getLayoutResource();
