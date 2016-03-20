652e8bd7bc5ebb0d7da70abbdf376ebb94fe8b41
Arne De Cock <arne.decock.1@student.kdg.be>
2016-03-20 23:49:41 +0100

    documentation invite

--- app/src/main/java/be/kdg/teame/kandoe/session/XmlClickable.java
+++ app/src/main/java/be/kdg/teame/kandoe/session/XmlClickable.java
@@ -2,6 +2,9 @@ package be.kdg.teame.kandoe.session;
 
 import android.view.View;
 
+/**
+ * Listener for {@code onClick} events from an xml {@link View}.
+ */
 public interface XmlClickable {
     void onRemoveInviteClick(View view);
 }
